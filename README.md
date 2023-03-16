070a6b566529703a54fdf475c25e195bd7fba9eb
mstr18 <mstr18@gmail.com>
2023-03-15 21:45:31 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Pagina simples de pesquisa dinâmica que direciona links
+
+A idéia é criar um repositório para pesquisa e base de conhecimento a partir do modelo
+usado nessa página e incrementá-lo
---
+++ ssearch.py
@@ -0,0 +1,24 @@
+from flask import Flask,  request, jsonify, render_template
+
+app = Flask(__name__)
+
+def page(result):
+    # Aqui você pode usar o resultado para gerar a página desejada
+    return f'<h1>Página para o resultado {result}</h1>'
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/search', methods=['POST'])
+def search():
+    search_term = request.form['search']
+    # Execute a função de pesquisa aqui para obter todos os resultados
+    all_results = ['Result 1', 'Result 2', 'Result 3']
+    # Filtrar os resultados com base na entrada do usuário
+    results = [result for result in all_results if result.lower().startswith(search_term.lower())]
+    return jsonify(results)
+
+if __name__ == '__main__':
+    app.run(debug=True)
